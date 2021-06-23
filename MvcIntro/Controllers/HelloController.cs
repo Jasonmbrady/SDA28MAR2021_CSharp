efafaed23b72c453fd97e1d6ffb903d2a85aad1a
Jason M Brady <jasonmbrady42@gmail.com>
2021-06-23 11:34:36 +0300

    mvc intro

---
+++ MvcIntro/Controllers/HelloController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcIntro.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("pizza/{topping}")]
+        public string Pizza(string topping)
+        {
+            return $"{topping} Pizza Time!";
+        }
+
+        [HttpGet("pizza/yuck")]
+        public string PizzaYuck()
+        {
+            return "I hate pizza!";
+        }
+    }
+}
