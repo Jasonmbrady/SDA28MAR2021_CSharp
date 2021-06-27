6e0905b2c5d7b84e9dd26df9218ff13b064c1cf3
Jason M Brady <jasonmbrady42@gmail.com>
2021-06-27 10:43:45 +0300

    git add *

---
+++ Partials/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+
+
+namespace Partials.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+       public ViewResult Index()
+       {
+        //    HttpContext.Session.SetString("Username", "J-Dawg");
+        //    HttpContext.Session.SetInt32("Age", 42);
+        //    string LocalName = HttpContext.Session.GetString("Username");
+        //    int? IntVariable = HttpContext.Session.GetInt32("Age");
+           TempData["Name"] = "J-Dawg";
+           TempData["Age"] = 42;
+           ViewBag.Username = TempData["Name"];
+           ViewBag.UserAge = TempData["Age"];
+           HttpContext.Session.Clear();
+           return View();
+       }
+    }
+}
---
+++ Partials/Models/Clock.cs
@@ -0,0 +1,8 @@
+using System;
+namespace Partials.Models
+{
+    public class Clock
+    {
+        public DateTime time = DateTime.Now;
+    }
+}
